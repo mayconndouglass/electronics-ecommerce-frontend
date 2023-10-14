e44583ab8060938a65c4e7312c14f55cf6dad2c5
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-10-14 16:42:41 -0300

    style: add responsive Experience section

--- src/pages/Home/components/Experience/index.tsx
+++ src/pages/Home/components/Experience/index.tsx
@@ -5,51 +5,54 @@ import { ImHeadphones } from 'react-icons/Im'
 import headsetWhite from '../../../../../public/assets/images/headset-white.png'
 import { AnimatedButton } from '../../../../components/AnimatedButton'
 import { Title } from '../../../../components/Title'
+import { Center } from '../../../../components/Center'
 
 export const Experience = () => {
   return (
     <S.Container>
-      <div className="info">
-        <Tag
-          icon={<ImHeadphones />}
-          title='Não Perca!!'
-          color={'#ff497c'}
-        />
+      <Center>
+        <div className="info">
+          <Tag
+            icon={<ImHeadphones />}
+            title='Não Perca!!'
+            color={'#ff497c'}
+          />
 
-        <Title fontSize={3}>
-          Aprimore Sua <br />Experiência Musical
-        </Title>
+          <Title fontSize={3}>
+            Aprimore Sua <br />Experiência Musical
+          </Title>
 
-        <div className="countdown-container">
-          <div className="countdown">
-            <span>0</span>
-            <p>Dias</p>
+          <div className="countdown-container">
+            <div className="countdown">
+              <span>0</span>
+              <p>Dias</p>
+            </div>
+            <div className="countdown">
+              <span>00</span>
+              <p>Hrs</p>
+            </div>
+            <div className="countdown">
+              <span>00</span>
+              <p>Min</p>
+            </div>
+            <div className="countdown">
+              <span>00</span>
+              <p>Seg</p>
+            </div>
           </div>
-          <div className="countdown">
-            <span>00</span>
-            <p>Hrs</p>
-          </div>
-          <div className="countdown">
-            <span>00</span>
-            <p>Min</p>
-          </div>
-          <div className="countdown">
-            <span>00</span>
-            <p>Seg</p>
-          </div>
+
+          <AnimatedButton
+            title={'Confira Agora!'}
+            color='white'
+            background='#3577F0'
+            padding={{ vertical: 1, horizontal: 2.375 }}
+          />
         </div>
 
-        <AnimatedButton
-          title={'Confira Agora!'}
-          color='white'
-          background='#3577F0'
-          padding={{ vertical: 1, horizontal: 2.375 }}
-        />
-      </div>
-
-      <div className="image">
-        <img src={headsetWhite} alt="Headset White" />
-      </div>
+        <div className="image">
+          <img src={headsetWhite} alt="Headset White" />
+        </div>
+      </Center>
     </S.Container>
   )
 }
--- src/pages/Home/components/Experience/styles.ts
+++ src/pages/Home/components/Experience/styles.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components'
 
 export const Container = styled.section`
-  display: flex;
-
-  max-width: 1320px;
   margin: 95px auto 0 auto;
-  padding: 65px 80px;
-  border-radius: 6px;
 
-  background-color: #F6F7FB;
+  & > div {
+    display: flex;
+    padding: 4rem 5rem;
+    border-radius: 6px;
+
+    background-color: #F6F7FB;
+  }
 
   h2 {
-    margin: 10px 0 30px 0;
+    margin: 0.625rem 0 2rem 0;
   }
 
   .info {
@@ -35,13 +36,13 @@ export const Container = styled.section`
       justify-content: center;
       flex-direction: column;
 
-      background-color: white;
+      background-color: ${props => props.theme.white};
       border-radius: 50%;
 
       span {
         font-size: 1.5rem;
-        font-weight: 500;
-        color: #000;
+        font-weight: ${props => props.theme['font-w-medium']};
+        color: ${props => props.theme.black};
         line-height: 1;
         margin-bottom: 5px;
       }
@@ -49,13 +50,85 @@ export const Container = styled.section`
       p {
         line-height: 1;
         font-size: 0.875rem;
-        font-weight: 500;
-        color: #777;
+        font-weight: ${props => props.theme['font-w-medium']};
+        color: ${props => props.theme['gray-500']};
       }
     }
   }
 
   .image {
     margin-top: -160px;
+  }
+
+  @media screen and (max-width: 990px) {
+    margin: 0 auto;
+
+    & > div {
+      flex-direction: column;
+      align-items: center;
+      gap: 4rem;
+      padding: 4rem 0;
+    }
+
+    h2 {
+      font-size: 2.5rem;
+      text-align: center;
+    }
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      width: 100%;
+
+      span {
+        justify-content: center;
+      }
+
+      & > div:last-child {
+        a {
+          padding: 0.75rem 1.56rem;
+        }
+      }
+    }
+
+    .image {
+      margin-top: 0;
+    }
+  }
+
+  @media screen and (max-width: 750px) {
+    h2 {
+      font-size: 1.75rem;
+    }
+
+    .image {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 90%;
+      }
+    }
+
+    .countdown-container {
+      .countdown {
+        width: 60px;
+        height: 60px;
+
+        span {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 350px) {
+    .countdown-container {
+      .countdown {
+        width: 50px;
+        height: 50px;
+      }
+    }
   }
 `
