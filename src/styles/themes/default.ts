f87c0add168af92e728ba65eb90949e990aa4fa8
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-10-17 12:35:12 -0300

    chore: add theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,7 +1,18 @@
 export const defaultTheme = {
   white: '#fff',
+  black: '#000',
 
   'pink-600': '#ff497c',
+  'yellow-300': '#ffdc60',
+  'yellow-400': '#ffa800',
+  'gray-100': '#f9f3f0',
+  'gray-500': '#777777',
+  'gray-900': '#292930',
+
+  'blue-600': '#0d6efd',
+  'blue-400': '#3577F0',
+  'slate-50': '#F6F7FB',
+  'slate-300': '#d6d6d6',
 
   'font-w-egular': 400,
   'font-w-medium': 500,
@@ -9,4 +20,4 @@ export const defaultTheme = {
   'font-w-bold': 700,
   'font-w-extra-bold': 800,
   'font-w-black': 900,
-}
+} as const
