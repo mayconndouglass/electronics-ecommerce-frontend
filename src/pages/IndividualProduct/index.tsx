788741e6c14966909a716ffb62cd3804ce3faeb9
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-11-03 16:06:32 -0300

    feat: add individual product page

---
+++ src/pages/IndividualProduct/index.tsx
@@ -0,0 +1,23 @@
+import * as S from './styles.ts'
+
+import { Header } from '../../components/Header/index.tsx'
+import { Banner } from './components/Banner/index.tsx'
+import { Promotions } from '../../components/Promotions/index.tsx'
+import { Footer } from '../../components/Footer/index.tsx'
+import { ToSign } from '../../components/ToSign/index.tsx'
+import { ProductDetails } from './components/ProductDetails/index.tsx'
+
+export const IndividualProduct = () => {
+  return (
+    <S.Container>
+      <Header />
+      <main>
+        <Banner />
+        <ProductDetails />
+        <Promotions $typeCard='normal' />
+        <ToSign />
+      </main>
+      <Footer />
+    </S.Container>
+  )
+}
