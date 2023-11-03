9fbe08afdd6fc5d012ed9edc7ab8fe43d66abfe7
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-11-03 16:05:09 -0300

    feat: add Banner component

---
+++ src/pages/IndividualProduct/components/Banner/index.tsx
@@ -0,0 +1,14 @@
+import { Center } from '../../../../components/Center'
+import * as S from './styles'
+
+export const Banner = () => {
+  return (
+    <S.Container >
+      <Center>
+        <p>
+          ESTUDANTE GANHA 10% DE DESCONTO: <a href="#">GARANTA SUA OFERTA</a>
+        </p>
+      </Center>
+    </S.Container >
+  )
+}
